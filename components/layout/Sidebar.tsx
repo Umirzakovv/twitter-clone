120d6ca092e450c77c1379bcc6ed8945b85fcdbe
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2024-01-05 10:41:13 +0500

    first commit

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,53 @@
+import SidebarLogo from "./SidebarLogo";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
+import { FaUser } from "react-icons/fa";
+import SidebarItem from "./SidebarItem";
+import SidebarTweetButton from "./SidebarTweetButton";
+const Sidebar = () => {
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: "Home",
+      href: "/",
+    },
+    {
+      icon: BsBellFill,
+      label: "Notifications",
+      href: "/notifications",
+      // auth: true,
+      // alert: currentUser?.hasNotification
+    },
+    {
+      icon: FaUser,
+      label: "Profile",
+      href: `/users/123`,
+      // auth: true,
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              // alert={item.alert}
+              // auth={item.auth}
+              href={item.href}
+              icon={item.icon}
+              label={item.label}
+            />
+          ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+          <SidebarTweetButton />
+          {/* {currentUser && <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />}
+        <SidebarTweetButton /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
